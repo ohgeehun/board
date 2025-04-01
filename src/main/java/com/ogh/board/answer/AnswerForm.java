276686c8281183943fdd1096afffd220bc43d2a5
ohgeehun <ohgeehun90@naver.com>
2025-04-01 16:11:49 +0900

    20250401_회원가입이전까지완료료

--- src/main/java/com/ogh/board/answer/AnswerForm.java
+++ src/main/java/com/ogh/board/answer/AnswerForm.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AnswerForm {
-    
     @NotEmpty(message = "내용은 필수항목입니다.")
     private String content;
 }
--- src/main/java/com/ogh/board/question/QuestionController.java
+++ src/main/java/com/ogh/board/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,7 +44,7 @@ public class QuestionController {
     // public String questionCreate() {
     //     return "question_form";
     // }
-    @RequestMapping(value = "/create")
+    @GetMapping(value = "/create")
     public String questionCreate(QuestionForm questionForm) {
         // QuestionForm questionForm : question_form.html에서 사용될 객체
         return "question_form";
